86496168498281c37134c05f602cf0c8c012426c
Jan Mlatec <tahicz@gmail.com>
2023-09-27 16:45:20 +0200

    Update config

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tahicz\SymfonyStatusLoggerBundle\Controller;
+namespace Tahicz\SymfonyStatusLoggerBundle;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
