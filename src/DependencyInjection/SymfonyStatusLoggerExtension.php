8c286c8373f6737b60d1bfecc6cca54b84a92acb
Jan Mlatec <tahicz@gmail.com>
2023-09-29 09:58:21 +0200

    Update config

--- src/DependencyInjection/SymfonyStatusLoggerExtension.php
+++ src/DependencyInjection/SymfonyStatusLoggerExtension.php
@@ -5,7 +5,6 @@ namespace Tahicz\SymfonyStatusLoggerBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SymfonyStatusLoggerExtension extends Extension
@@ -15,11 +14,9 @@ class SymfonyStatusLoggerExtension extends Extension
 	 */
 	public function load(array $configs, ContainerBuilder $container): void
 	{
-		$loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
-		$loader->load('status-logger-config.xml');
-
-		$loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+		$loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources'));
 		$loader->load('services.yaml');
+		$loader->load('route.yaml');
 
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
